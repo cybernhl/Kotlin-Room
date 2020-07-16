0683585c138b2df9648868607b736e0e69fbf42f
liukai <liukai2530533@163.com>
2020-07-16 23:01:34 +0800

    Demo运行

--- standalone/demorunalone/src/main/java/com/guadou/kt_demo/DemoApplication.kt
+++ standalone/demorunalone/src/main/java/com/guadou/kt_demo/DemoApplication.kt
@@ -1,10 +1,7 @@
 package com.guadou.kt_demo
 
-import com.guadou.cpt_main.di.mainModule
 import com.guadou.lib_baselib.base.BaseApplication
-import org.koin.android.ext.koin.androidContext
-import org.koin.android.ext.koin.androidLogger
-import org.koin.core.context.startKoin
+
 
 class DemoApplication : BaseApplication() {
 
@@ -12,10 +9,10 @@ class DemoApplication : BaseApplication() {
         super.onCreate()
 
         //初始化Koin-只能在App模块初始化
-        startKoin {
-            androidLogger()
-            androidContext(this@DemoApplication)
-            modules(mainModule)
-        }
+//        startKoin {
+//            androidLogger()
+//            androidContext(this@DemoApplication)
+//            modules(mainModule)
+//        }
     }
 }
