ccdb76281d613bf7216fdae234c528640ebde428
liukai <liukai2530533@163.com>
2020-07-16 22:04:31 +0800

    rv基于easyadapter的拓展

--- cs_baselib/src/main/java/com/guadou/lib_baselib/ext/RecyclerViewExt.kt
+++ cs_baselib/src/main/java/com/guadou/lib_baselib/ext/RecyclerViewExt.kt
@@ -4,6 +4,10 @@ import android.graphics.Color
 import android.graphics.drawable.GradientDrawable
 import android.view.View
 import androidx.recyclerview.widget.*
+import com.lxj.easyadapter.EasyAdapter
+import com.lxj.easyadapter.ItemDelegate
+import com.lxj.easyadapter.MultiItemTypeAdapter
+import com.lxj.easyadapter.ViewHolder
 
 /**
  * Description: RecyclerView扩展
@@ -14,10 +18,14 @@ import androidx.recyclerview.widget.*
  * 设置分割线
  * @param color 分割线的颜色，默认是#DEDEDE
  * @param size 分割线的大小，默认是1px
- * @param isReplace 是否覆盖之前的ItemDecoration，默认是true
+ * @param isReplace 是否覆盖之前的ItemDecoration，默认是false
  *
  */
-fun RecyclerView.divider(color: Int = Color.parseColor("#DEDEDE"), size: Int = 1, isReplace: Boolean = false): RecyclerView {
+fun RecyclerView.divider(
+    color: Int = Color.parseColor("#DEDEDE"),
+    size: Int = 1,
+    isReplace: Boolean = false
+): RecyclerView {
     val decoration = DividerItemDecoration(context, orientation)
     decoration.setDrawable(GradientDrawable().apply {
         setColor(color)
@@ -32,6 +40,7 @@ fun RecyclerView.divider(color: Int = Color.parseColor("#DEDEDE"), size: Int = 1
 }
 
 
+//设置垂直的Manager,默认不是瀑布流
 fun RecyclerView.vertical(spanCount: Int = 0, isStaggered: Boolean = false): RecyclerView {
     layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
     if (spanCount != 0) {
@@ -43,6 +52,7 @@ fun RecyclerView.vertical(spanCount: Int = 0, isStaggered: Boolean = false): Rec
     return this
 }
 
+//设置水平的Manager,默认不是瀑布流
 fun RecyclerView.horizontal(spanCount: Int = 0, isStaggered: Boolean = false): RecyclerView {
     layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
     if (spanCount != 0) {
@@ -54,6 +64,7 @@ fun RecyclerView.horizontal(spanCount: Int = 0, isStaggered: Boolean = false): R
     return this
 }
 
+//获取到布局的方向
 inline val RecyclerView.orientation
     get() = if (layoutManager == null) -1 else layoutManager.run {
         when (this) {
@@ -63,4 +74,84 @@ inline val RecyclerView.orientation
             else -> -1
         }
     }
+
+// ============================  Adapter的使用基于XPopup中的EasyAdapter ↓  =============================
+
+//获取到adapter的数据
+inline val RecyclerView.data
+    get() = (adapter as EasyAdapter<*>).data
+
+//设置自定义的简易数据适配器
+fun <T> RecyclerView.bindData(
+    data: List<T>,
+    layoutId: Int,
+    bindFn: (holder: ViewHolder, t: T, position: Int) -> Unit
+): RecyclerView {
+    adapter = object : EasyAdapter<T>(data, layoutId) {
+        override fun bind(holder: ViewHolder, t: T, position: Int) {
+            bindFn(holder, t, position)
+        }
+    }
+    return this
+}
+
+/**
+ * 必须在bindData之后调用，并且需要hasHeaderOrFooter为true才起作用
+ */
+fun RecyclerView.addHeader(headerView: View): RecyclerView {
+    adapter?.apply {
+        (this as EasyAdapter<*>).addHeaderView(headerView)
+    }
+    return this
+}
+
+/**
+ * 必须在bindData之后调用，并且需要hasHeaderOrFooter为true才起作用
+ */
+fun RecyclerView.addFooter(footerView: View): RecyclerView {
+    adapter?.apply {
+        (this as EasyAdapter<*>).addFootView(footerView)
+    }
+    return this
+}
+
+//Item的点击
+fun <T> RecyclerView.itemClick(block: (data: List<T>, holder: RecyclerView.ViewHolder, position: Int) -> Unit): RecyclerView {
+    adapter?.apply {
+        (adapter as MultiItemTypeAdapter<*>).setOnItemClickListener(object :
+            MultiItemTypeAdapter.SimpleOnItemClickListener() {
+            override fun onItemClick(view: View, holder: RecyclerView.ViewHolder, position: Int) {
+                block(data as List<T>, holder, position)
+            }
+        })
+    }
+    return this
+}
+
+//Item的长按回调
+fun <T> RecyclerView.itemLongClick(block: (data: List<T>, holder: RecyclerView.ViewHolder, position: Int) -> Unit): RecyclerView {
+    adapter?.apply {
+        (adapter as MultiItemTypeAdapter<*>).setOnItemClickListener(object :
+            MultiItemTypeAdapter.SimpleOnItemClickListener() {
+            override fun onItemLongClick(
+                view: View,
+                holder: RecyclerView.ViewHolder,
+                position: Int
+            ): Boolean {
+                block(data as List<T>, holder, position)
+                return super.onItemLongClick(view, holder, position)
+            }
+        })
+    }
+    return this
+}
+
+
+//多布局（不推荐这么使用，- 推荐使用BRVAH）
+fun <T> RecyclerView.multiTypes(data: List<T>, itemDelegates: List<ItemDelegate<T>>): RecyclerView {
+    adapter = MultiItemTypeAdapter<T>(data).apply {
+        itemDelegates.forEach { addItemDelegate(it) }
+    }
+    return this
+}
 
